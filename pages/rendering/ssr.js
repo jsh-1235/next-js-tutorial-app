f6b4c56384bdec5ff3f8fd5f3485faf0caa54090
jsh-1235 <jsh-1235@hanmail.net>
2023-03-07 19:06:54 +0900

    update

--- pages/rendering/ssr.js
+++ pages/rendering/ssr.js
@@ -29,29 +29,18 @@ function SSR({ posts }) {
   );
 }
 
-// export async function getServerSideProps() {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-
-//     const posts = await response.json();
-
-//     // console.log(posts);
-
-//     return {
-//       props: {
-//         posts,
-//       },
-//     };
-//   } catch (error) {
-//     console.log(error);
-//     return { props: {} };
-//   }
-// }
-
 export async function getServerSideProps() {
   try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const posts = await response.json();
+
+    // console.log(posts);
+
     return {
-      props: {},
+      props: {
+        posts,
+      },
     };
   } catch (error) {
     console.log(error);
